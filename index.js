faa98aa428effdf553b82cf14a8349596cca074e
Roberto Jacquez <rjqz92@gmail.com>
2021-10-11 23:18:45 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+//Code your solutions in this file
+for (let age =30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+
+
+function writeCards(names,eventName){
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        //debugger;
+    }
+    return messages;
+}
+
+function countDown(positiveNumber){
+    while (positiveNumber>=0){
+        console.log(positiveNumber--);
+    }
+}
